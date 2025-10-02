83997ca3eb1f57bc24b2f64d63ccef2d77f4eb63
Kenny <kennykotten@gmail.com>
2025-10-02 14:02:37 -0700

    clean up main

--- Board.cpp
+++ Board.cpp
@@ -1,25 +1,23 @@
-#include <iostream>
 #include "Board.h"
-#include <vector>
+
+#include <iostream>
 #include <string>
+#include <vector>
 
 // TODO: consider deep copy or not
 Board::Board(std::vector<std::vector<std::string>> board) {
     this->rows = board.size();
     this->cols = board[0].size();
     this->board = board;
-    // std::cout << "board constuctor: row: " << this->rows << "cols: " << this->cols << "\n";
+    // std::cout << "board constuctor: row: " << this->rows << "cols: " <<
+    // this->cols << "\n";
 }
 
-std::string Board::getCellValue(int r, int c) const {
-    return board[r][c];
-}
+std::string Board::getCellValue(int r, int c) const { return board[r][c]; }
 
 void Board::setCellValue(int r, int c, std::string newStr) {
     board[r][c] = newStr;
 }
-
-
 
 void Board::markVisited(int r, int c) {
     board[r][c] = "*";
@@ -27,46 +25,37 @@ void Board::markVisited(int r, int c) {
 }
 
 bool Board::isVisited(int r, int c) const {
-    if (board[r][c] == "*")
-        return true;
+    if (board[r][c] == "*") return true;
     return false;
 }
 
-bool Board::isValidMove(int r, int c) const { 
-    if (!isInBounds(r,c)) 
-        return false;
-    if (isVisited(r,c))
-        return false;
+bool Board::isValidMove(int r, int c) const {
+    if (!isInBounds(r, c)) return false;
+    if (isVisited(r, c)) return false;
     return true;
 }
-    
+
 bool Board::isInBounds(int r, int c) const {
-    if (r < 0 || r >= rows)
-        return false;
-    if (c < 0 || c >= cols)
-        return false;
+    if (r < 0 || r >= rows) return false;
+    if (c < 0 || c >= cols) return false;
 
     return true;
 }
 
+int Board::getRowCount() const { return rows; }
 
-int Board::getRowCount() const {
-    return rows;
-}
-
-int Board::getColCount() const {
-    return cols;
-}
+int Board::getColCount() const { return cols; }
 
 void Board::printBoard() const {
+    std::cout << "\n";
     for (int r = 0; r < rows; r++) {
         for (int c = 0; c < cols; c++) {
             std::cout << board[r][c] << " ";
 
             // for good formatting for both 1 and 2 length squares
-            if (board[r][c].size() == 1)
-                std::cout << " ";
+            if (board[r][c].size() == 1) std::cout << " ";
         }
         std::cout << "\n";
     }
+    std::cout << "\n";
 }
--- DictMain.cpp
+++ DictMain.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-// #include <vector>
+#include <vector>
 #include <set>
 
 #include "Dict.h"
@@ -8,91 +8,9 @@
 #include "Solver.h"
 #include "Board.h"
 
-void dfs(int row, int col, Dict::Node* currNode, std::string& currPrefix,
-         std::string board[2][3], std::set<FoundWord> &wordsFound, std::vector<std::pair<int, int>> &currPath) {
-    // bounds
-    if (row < 0 || col < 0) return;
-    if (row >= 2 || col >= 3) return; 
-
-    // "*" means cell is visited
-    if (board[row][col] == "*") return;
-
-    std::string cellStr = board[row][col];
-    Dict::Node* next = currNode;
-
-    for (char ch : cellStr) {
-        int idx = ch - 'a';
-        if (idx < 0 || idx >= 26) return;
-        next = next->letterPointers[idx];
-        if (!next) return;  // ex: ensures both "q" and "u" are in path for "qu"
-    }
-    board[row][col] = "*";  // mark visited
-
-    currPath.emplace_back(row, col);
-    currPrefix += cellStr;
-
-    if (next->isWord) {
-        // wordsFound.insert(currPrefix);
-        wordsFound.insert(FoundWord(currPrefix, next->definition, currPath));
-    }
-
-    dfs(row - 1, col, next, currPrefix, board, wordsFound, currPath); 
-    dfs(row - 1, col + 1, next, currPrefix, board, wordsFound, currPath); 
-    dfs(row, col + 1, next, currPrefix, board, wordsFound, currPath);    
-    dfs(row + 1, col + 1, next, currPrefix, board, wordsFound, currPath);
-    dfs(row + 1, col, next, currPrefix, board, wordsFound, currPath);   
-    dfs(row + 1, col - 1, next, currPrefix, board, wordsFound, currPath);
-    dfs(row, col - 1, next, currPrefix, board, wordsFound, currPath);   
-    dfs(row - 1, col - 1, next, currPrefix, board, wordsFound, currPath);
-
-    // backtrack
-    currPrefix.resize(currPrefix.size() - cellStr.size());
-    currPath.pop_back();
-    board[row][col] = cellStr; 
-}
 
 int main() {
-    std::cout << "Main starting" << std::endl;
-
     Dict d1("./dictionaries/smalldict1.txt");
-
-    std::cout << "word count: " << d1.getWordCount();
-    std::cout << "\n\n\n";
-
-
-    std::string board[2][3];
-    board[0][0] = "qu";
-    board[0][1] = "a";
-    board[0][2] = "t";
-
-    board[1][0] = "x";
-    board[1][1] = "c";
-    board[1][2] = "k";
-
-
-    Dict::Node* root = d1.getRoot();
-    std::string currPrefix = "";
-
-    std::vector<std::pair<int, int>> currPath;
-    // std::set<std::string> wordsFound;
-    std::set<FoundWord> wordsFound;
-
-    for (int r = 0; r < 2; r++) {
-        for (int c = 0; c < 3; c++) {
-            dfs(r, c, root, currPrefix, board, wordsFound, currPath);
-        }
-    }
-
-    for (auto& word : wordsFound) {
-        word.print();
-    }
-
-    std::cout << "\n\n\n" << std::endl;
-
-
-    std::cout << "-----------\n";
-    std::cout << "-----------\n";
-    std::cout << "-----------\n";
 
     std::vector<std::vector<std::string>> b1input = {
         {"qu", "a", "t"},
@@ -107,9 +25,6 @@ int main() {
     std::set<FoundWord> answers = s1.getFoundWords(b1);
     for (auto word : answers)
         word.print();
-
-
-
 
 
 }
